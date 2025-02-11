6523cb5cc3dabb0a789dfaf557b3fab11e81e74e
Chrystopher <cthomas09252003@berkeley.edu>
2025-02-11 11:31:39 -0800

    Working on challenge positions map

--- SquirrelsGoNutsGame/GameMechanics.py
+++ SquirrelsGoNutsGame/GameMechanics.py
@@ -24,6 +24,7 @@ class FlowerPieces():
         self.color = color
 
 
+
 class SquirrelsGoNuts():
     # board is 4 x 4
     board = [
@@ -36,17 +37,23 @@ class SquirrelsGoNuts():
     whiteSquirrel = SquirrelPieces("white", "rectangle", 2, False)  # size 2; hole at the very end
     blondSquirrel = SquirrelPieces("blond", "L", 4, True)  # size 4 in an L shape; flower at the end
     graySquirrel = SquirrelPieces("gray", "rectangle", 2, False)  # size 2; hole at the very end
-    redSquirrel = SquirrelPieces("red", "L", 2, False)  # size 2; hole at the very end
+    orangeSquirrel = SquirrelPieces("red", "L", 2, False)  # size 2; hole at the very end
 
     flowerPiece = FlowerPieces("yellow")  # size 1; flower on top
 
     acornPieces = ["a", "a", "a", "a" "a"]  # 5 pieces total --> placed in board's "o"
 
-    def __init__(self, challengePosition):
+    # maps integer (difficulty level) to squirrel coordinates
+    challengePositions = { 1: (1, )
+
+    }
+
+
+    def __init__(self, difficultyLevel):
         """ if the challenge position determines how many squirrels you'll have, how can I represent
         that diagram in code? """
-        self.challengePosition = challengePosition
         SquirrelsGoNuts.whiteSquirrel.acorn = SquirrelsGoNuts.acornPieces[0]
+        self.difficultyLevel = difficultyLevel
 
 
 
